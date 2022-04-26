7e0207240fe28d70b06389a3fc530bc18ed3b95b
vidit-shah <vidit.s@somaiya.edu>
2022-04-26 15:29:16 +0530

    updated

--- backend/project/blog/urls.py
+++ backend/project/blog/urls.py
@@ -4,4 +4,8 @@ from .import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('signup/', views.signup, name='signup'),
+    path('vedio/', views.vedio, name='vedio-1'),
+    path('search/', views.search, name='search'),
 ]
--- backend/project/blog/views.py
+++ backend/project/blog/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/login.html')
+
+
+def dashboard(request):
+    return render(request, 'blog/login.html')
+
+
+def signup(request):
+    return render(request, 'blog/login.html')
+
+
+def search(request):
+    return render(request, 'blog/login.html')
+
+
+def vedio(request):
+    return render(request, 'blog/login.html')
